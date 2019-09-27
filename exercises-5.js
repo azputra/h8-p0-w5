3ca8c21e897e653a087e513a2045127bc27ed2cc
azputra <ahmadzp102@gmail.com>
2019-09-27 14:13:34 +0700

    :aanchor:

--- exercises-5.js
+++ exercises-5.js
@@ -1,11 +1,22 @@
 function kaliTerusRekursif(angka) {
-    // you can only write your code here!
-    
+  // you can only write your code here!
+  let strAngka = String(angka);
+  let checkSisa = 1
+  for (let i = 0; i < strAngka.length; i++) {
+    checkSisa *= Number(strAngka[i])
+    // console.log(checkSisa)
   }
-  
-  // TEST CASES
-  console.log(kaliTerusRekursif(66)); // 8
-  console.log(kaliTerusRekursif(3)); // 3
-  console.log(kaliTerusRekursif(24)); // 8
-  console.log(kaliTerusRekursif(654)); // 0
-  console.log(kaliTerusRekursif(1231)); // 6
+  var sisaAngka = String(checkSisa)
+  if (sisaAngka.length === 1) {
+    return Number(sisaAngka)
+  } else {
+    return kaliTerusRekursif(sisaAngka)
+  }
+}
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
